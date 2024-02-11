2b8df7fe0e98d2818f16a196c50e4cd5374cfd32
syed fahad ali <155745867+syedfahadali399@users.noreply.github.com>
2024-02-11 22:47:54 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+// Assignment #1
+btn.addEventListener('click',()=> {
+    alert('Button Is Clicked')
+})
+
+// Assignment #2
+div.addEventListener('mouseover', ()=> {
+    div.style.backgroundColor = 'blue'
+})
+
+div.addEventListener('mouseout', ()=> {
+    div.style.backgroundColor = 'lightgreen'
+})
